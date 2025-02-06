a55ad351a2ffce5c9aa64907bb3916dc4cce3659
WaseeKhan <mdvaseem014@gmail.com>
2025-02-06 22:07:20 +0530

    fetch all fies with created date

--- housekeeping_x_day.py
+++ housekeeping_x_day.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import datetime
 req_path = input("Enter path: ")
 if not os.path.exists(req_path):
    print("Please enter a valid path")
@@ -8,6 +9,11 @@ if os.path.isfile(req_path):
    print("Please provide direvtory path")
    sys.exit(2)
 for each_file in os.listdir(req_path):
-   print(each_file)
+   #fetch path with filename[complete path]
+   complete_path = os.path.join(req_path, each_file) 
+   #check for files
+   if os.path.isfile(complete_path):
+      print(complete_path, datetime.datetime.fromtimestamp(os.path.getctime(complete_path)))
+
 
 
