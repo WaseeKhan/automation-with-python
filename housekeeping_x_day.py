03ed5f46efca634181bfb5211ce10789ffb3a650
WaseeKhan <mdvaseem014@gmail.com>
2025-02-06 22:56:16 +0530

    Script is ready to delete x days file from given path

--- housekeeping_x_day.py
+++ housekeeping_x_day.py
@@ -1,19 +1,28 @@
 import os
 import sys
 import datetime
+
 req_path = input("Enter path: ")
+file_age = 2
 if not os.path.exists(req_path):
    print("Please enter a valid path")
    sys.exit(1)
 if os.path.isfile(req_path):
    print("Please provide direvtory path")
    sys.exit(2)
+# get current date
+cur_date = datetime.datetime.now()
 for each_file in os.listdir(req_path):
    #fetch path with filename[complete path]
    complete_path = os.path.join(req_path, each_file) 
    #check for files
    if os.path.isfile(complete_path):
-      print(complete_path, datetime.datetime.fromtimestamp(os.path.getctime(complete_path)))
-
+      #get file create datetime
+      file_create_datetime = datetime.datetime.fromtimestamp(os.path.getctime(complete_path))
+      # print(complete_path, file_create_datetime)
+      diff_days = (cur_date - file_create_datetime).days
+      if diff_days > file_age:
+         os.remove(complete_path)
+         print(complete_path, diff_days)
 
 
