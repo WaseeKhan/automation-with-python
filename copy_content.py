64d0d597a110deb036c95165c874a9852363dc79
WaseeKhan <mdvaseem014@gmail.com>
2025-02-10 00:32:58 +0530

    Script completed v1

---
+++ copy_content.py
@@ -0,0 +1,15 @@
+#! /usr/bin/python3
+source_path = "/Users/waseem/Developer/python-lab/Hello.py"
+destination_path = "/Users/waseem/Developer/python-lab/myTest/newHello.py"
+
+open_source_file = open(source_path, 'r')
+read_source_content = open_source_file.read()
+open_source_file.close()
+print("***** Printing file contents start here *****")
+print(read_source_content)
+print("***** Printing file contents end here *****")
+
+open_dest_file = open(destination_path, 'w')
+write_dest_file = open_dest_file.write(read_source_content)
+print("Content Copied Successfuly!!!")
+open_dest_file.close()
